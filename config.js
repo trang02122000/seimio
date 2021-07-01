079e9edfddb4086ef289e963da666e627e3345e9
trang02122000 <86784223+trang02122000@users.noreply.github.com>
2021-07-01 20:42:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à <3',
+    introTitle: 'alo Lợn à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao anh cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'tiếp đi nekk',
+    btnIntro: 'next nekk',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'yêu em nhiều :))',
-    btnNo: 'yêu em khác dòi',
-    question:'trên thế giới có 7 tỷ người vì sao anh lại yêu em nhỉ??<3',
-    btnReply: 'anh nhỉ<3',
-    reply: 'vì em đáng yêu lắm ,ny anh lại còn ngoan nữa ;)) ',
-    mess: 'em biết mà:).Yêu anh nhiều nhiều 😘😘',
-    messDesc: 'hẹn gặp chú bộ đội của em<3 yêu.',
-    btnAccept: 'Okiiiii lun <3',
+    btnYes: 'không yêu đấm chớt <33',
+    btnNo: 'yêu em nhiều :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em?? <3',
+    btnReply: 'anh nhỉ <3',
+    reply: 'vì người yêu anh cute nekk,ngoan nữa nekk hihi <33333333',
+    mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
+    messDesc: 'hẹn gặp chú bộ đội của em <3.',
+    btnAccept: 'yêuuuu<3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
